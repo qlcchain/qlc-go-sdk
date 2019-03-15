038b21f818eff18cb0cbd4cfeace215de69473fe
zengchen221 <931891076@qq.com>
2019-03-15 16:56:30 +0800

    feat: ledger generate block

--- client_test.go
+++ client_test.go
@@ -7,3 +7,26 @@ import (
 func TestQLCClient_Version(t *testing.T) {
 	t.Log("test")
 }
+
+//func TestQLCClient_GenerateSendBlock(t *testing.T) {
+//	client, err := NewQLCClient("http://47.244.138.61:9375")
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	bp := api.APISendBlockPara{
+//		From:      common.GenesisAccountAddress,
+//		TokenName: "QLC",
+//		To:        mock.Address(),
+//		Amount:    types.Balance{Int: big.NewInt(int64(100))},
+//		Sender:    "100",
+//		Receiver:  "200",
+//	}
+//
+//	s, err := client.Ledger.GenerateSendBlock(&bp, nil)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	fmt.Println(s)
+//
+//}
--- module/mintage.go
+++ module/mintage.go
@@ -29,11 +29,3 @@ func (m *MintageApi) GetRewardBlock(input *types.StateBlock) (*types.StateBlock,
 func (m *MintageApi) GetWithdrawMintageData(tokenId types.Hash) ([]byte, error) {
 	return nil, nil
 }
-
-func (m *MintageApi) GetTokenInfoList(count int, index int) (*api.TokenInfoList, error) {
-	return nil, nil
-}
-
-func (m *MintageApi) GetTokenInfoById(tokenId types.Hash) (*api.ApiTokenInfo, error) {
-	return nil, nil
-}
