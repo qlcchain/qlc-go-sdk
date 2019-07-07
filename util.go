289d178d3b44fded68898123ff6685e540705248
zengchen221 <931891076@qq.com>
2019-07-07 16:21:05 +0800

    feat: modify extension type

--- util.go
+++ util.go
@@ -1,6 +1,8 @@
 package qlcchain
 
 import (
+	"math/big"
+
 	rpc "github.com/qlcchain/jsonrpc2"
 	"github.com/qlcchain/qlc-go-sdk/pkg/types"
 	"github.com/qlcchain/qlc-go-sdk/pkg/util"
@@ -25,20 +27,32 @@ func (u *UtilApi) Encrypt(raw string, passphrase string) (string, error) {
 	return util.Encrypt(raw, passphrase)
 }
 
+type APIBalance struct {
+	*big.Float
+}
+
+func (b *APIBalance) MarshalText() ([]byte, error) {
+	return []byte(b.String()), nil
+}
+
+func (b *APIBalance) String() string {
+	return b.Text('f', -1)
+}
+
 // RawToBalance transforms QLC amount from raw to unit
-func (u *UtilApi) RawToBalance(balance types.Balance, unit string) (types.Balance, error) {
-	var b types.Balance
+func (u *UtilApi) RawToBalance(balance types.Balance, unit string) (APIBalance, error) {
+	var b APIBalance
 	if err := u.client.Call(&b, "util_rawToBalance", balance, unit); err != nil {
-		return types.ZeroBalance, err
+		return APIBalance{big.NewFloat(0)}, err
 	}
 	return b, nil
 }
 
 // RawToBalance transforms token (not QLC) amount from raw
-func (u *UtilApi) RawToBalanceForToken(balance types.Balance, tokenName string) (types.Balance, error) {
-	var b types.Balance
+func (u *UtilApi) RawToBalanceForToken(balance types.Balance, tokenName string) (APIBalance, error) {
+	var b APIBalance
 	if err := u.client.Call(&b, "util_rawToBalance", balance, "", tokenName); err != nil {
-		return types.ZeroBalance, err
+		return APIBalance{big.NewFloat(0)}, err
 	}
 	return b, nil
 }
