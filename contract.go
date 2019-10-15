256a1ba53ae5d3b69a0bcce1d1200b2935c30c10
zengchen221 <931891076@qq.com>
2019-10-15 12:00:39 +0800

    feat: add sdk to get contract address and abi

--- contract.go
+++ contract.go
@@ -1,6 +1,9 @@
 package qlcchain
 
-import rpc "github.com/qlcchain/jsonrpc2"
+import (
+	rpc "github.com/qlcchain/jsonrpc2"
+	"github.com/qlcchain/qlc-go-sdk/pkg/types"
+)
 
 type ContractApi struct {
 	client *rpc.Client
@@ -9,6 +12,21 @@ type ContractApi struct {
 // NewContractApi creates contract module for client
 func NewContractApi(c *rpc.Client) *ContractApi {
 	return &ContractApi{client: c}
+}
+
+// GetAbiByContractAddress return contract abi by contract address
+func (c *ContractApi) GetAbiByContractAddress(address types.Address) (string, error) {
+	var r string
+	err := c.client.Call(&r, "contract_getAbiByContractAddress", address)
+	if err != nil {
+		return "", err
+	}
+	return r, nil
+}
+
+// ContractAddressList return all contract addresses
+func (c *ContractApi) ContractAddressList() []types.Address {
+	return types.ChainContractAddressList
 }
 
 // PackContractData parse a ABI interface and pack the given method name to conform the ABI.
